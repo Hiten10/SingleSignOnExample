7512704c341a6a24eefdf062fc03c4b9395ca421
GNP4 <GNP4@global.Tesco.org>
2017-08-23 11:22:47 +0530

    Add project files.

---
+++ SingleSignOnExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SingleSignOnExample.Startup))]
+namespace SingleSignOnExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
